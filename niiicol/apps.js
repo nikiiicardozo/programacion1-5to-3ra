5886b3a044a1ba73475102a374c1039a349f1c95
nikiiicardozo <152309090+nikiiicardozo@users.noreply.github.com>
2023-11-28 13:41:19 -0300

    subiendo los trabajos

---
+++ niiicol/apps.js
@@ -0,0 +1,48 @@
+// clase 1
+//este es un comentario de una linea
+/*comentario
+multilinea
+*/
+
+// las variables son una caja que almacena un valor
+var miNombre;
+
+// almacenar un valor en la variable creada
+miNombre = "nicol";
+
+// ejercicio 1:
+// crear una variable edad y guardarle un numero 
+
+var edad; // creamos la variable
+edad = 17; // la inicializamos
+console.log(edad); // esto muestra en la consola
+
+//ejercicio 2:
+// inicializar tres variables a,b y c con los valores
+// 5, 10 y "I am a"
+
+// // cambia solo el codigo debajo de esta linea
+var a = 5;
+var b = 10;
+var c = "I am a";
+// // cambia solo el codigo encima de esta linea
+
+a = a + 1;
+b = b + 5;
+c = c + "sting!";
+console.log(a);
+console.log(b);
+console.log(c);
+
+//clase 2
+var nombre = prompt('ingrese su nombre')
+console.log('hola mundo');
+
+// clase 3
+// ejercicio 3
+// calcular el area y el perimetro de un cuadrado
+let lado = 40;
+let area = lado * lado;
+let perimetro = lado * 4;
+console.log('el area de un cuadrado es: ',area);
+console.log('el perimetro de un cuadrado es:',perimetro);
